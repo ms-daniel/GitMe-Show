814e8d786ad0abe4adb17ce942f484c05bd88caa
Carlos Daniel <ms_danniel@outlook.com>
2024-07-01 23:18:40 -0300

    components e organizacao

--- Angular - Frontend/src/app/app.component.ts
+++ Angular - Frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BaseUiComponent } from './components/base-ui/base-ui.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Gitme Show';
 }
