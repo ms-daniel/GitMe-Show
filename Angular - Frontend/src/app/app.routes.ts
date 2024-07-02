b4b54f8cf4d376fab965905b52e296efa6a4b626
Carlos Daniel <ms_danniel@outlook.com>
2024-07-01 23:18:53 -0300

    rotas definidas

--- Angular - Frontend/src/app/app.routes.ts
+++ Angular - Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'found', component: ProfileComponent}
+];
