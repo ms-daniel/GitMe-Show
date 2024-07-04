9629efcfb0b26828dae722ed1cec6b743d4c557c
Carlos Daniel <ms_danniel@outlook.com>
2024-07-04 14:50:08 -0300

    endpoint. resolved #3

--- Laravel - Backend/routes/api.php
+++ Laravel - Backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/get', 'UserProfileController@get');
+
+
