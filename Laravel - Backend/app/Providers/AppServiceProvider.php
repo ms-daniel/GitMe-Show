388ba2b8fecc48425bc3b2cd9b902672718ef668
Carlos Daniel <ms_danniel@outlook.com>
2024-07-04 14:47:20 -0300

    injecao de dependencia #1

--- Laravel - Backend/app/Providers/AppServiceProvider.php
+++ Laravel - Backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,14 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Contracts\Repositories\UserProfileRepositoryInterface;
+use App\Repositories\UserProfileRepository;
+
+use App\Contracts\Services\UserProfileServiceInterface;
+use App\Services\UserProfileService;
+
+use GuzzleHttp\Client;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserProfileRepositoryInterface::class, UserProfileRepository::class);
+
+        $this->app->bind(UserProfileServiceInterface::class, UserProfileService::class);
+
+        $this->app->singleton(Client::class, function ($app) {
+            return new Client([
+            ]);
+        });
     }
 
     /**
