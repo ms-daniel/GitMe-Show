1c6d79a2405a7deacc141dfc5412c9e9a4a4b818
Carlos Daniel <ms_danniel@outlook.com>
2024-07-05 17:54:29 -0300

    Resgate de dados e uso no html #9

--- Angular - Frontend/src/app/pages/profile/profile.component.ts
+++ Angular - Frontend/src/app/pages/profile/profile.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { AccountBoxComponent } from '../../components/account-box/account-box.component';
+import { UserProfileService } from '../../services/user-profile.service';
+import { UserProfile } from '../../models/user-profile.model';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { UserFollow } from '../../models/user-follow.model';
+import { FollowsBoxComponent } from '../../components/follows-box/follows-box.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NavComponent, AccountBoxComponent],
+  imports: [
+    NavComponent, AccountBoxComponent,
+    CommonModule, FollowsBoxComponent
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
   avatarPath: string = '../../../assets/images/avatar.jpg';
+
+  urlProfile: string = '';
+
+  userProfile$ = new Observable<UserProfile | null>();
+
+  constructor(private userService: UserProfileService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.urlProfile = this.route.snapshot.paramMap.get('url') ?? '';
+
+    this.userProfile$ = this.userService.getUserProfile();
+  }
 }
