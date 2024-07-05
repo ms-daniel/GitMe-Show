5def7b9bf04cc29940759a4c06604ff69317131c
Carlos Daniel <ms_danniel@outlook.com>
2024-07-05 17:48:35 -0300

    toast #6

--- Angular - Frontend/src/app/app.config.ts
+++ Angular - Frontend/src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr({
+      preventDuplicates: true,
+      maxOpened: 2
+    })
+  ]
 };
