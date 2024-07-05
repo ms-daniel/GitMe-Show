f2a14591e023c6c0e403a94a77c9ecba8d30f134
Carlos Daniel <ms_danniel@outlook.com>
2024-07-05 17:52:54 -0300

    requisição de Followings, get de dados de user #8

--- Angular - Frontend/src/app/services/user-profile.service.ts
+++ Angular - Frontend/src/app/services/user-profile.service.ts
@@ -2,6 +2,10 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
 import { UserProfile } from "../models/user-profile.model";
+import { tap } from "rxjs/operators";
+import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
+import { Observable } from "rxjs/internal/Observable";
+import { UserFollow } from "../models/user-follow.model";
 
 
 @Injectable({
@@ -11,14 +15,27 @@ export class UserProfileService {
 
   private apiUrl = environment.api;
 
+  private userProfileSubject = new BehaviorSubject<UserProfile | null>(null);
+  private userProfile$ = this.userProfileSubject.asObservable();
+
   constructor(private httpClient: HttpClient){}
 
-  getUserProfile(urlProfile : string){
+  requestUserProfile(urlProfile : string){
     const params = new HttpParams().set('url', urlProfile);
-    return this.httpClient.get<UserProfile>(this.apiUrl + '/get', {params});
+    return this.httpClient.get<UserProfile>(this.apiUrl + '/get', {params}).pipe(
+      tap((profile: UserProfile) => {
+        this.userProfileSubject.next(profile);
+      })
+    );
   }
 
-  getUserFollowings(){
+  getUserProfile(): Observable<UserProfile | null>{
+    return this.userProfile$;
+  }
 
+  getUserFollowings(urlProfile: string){
+    const params = new HttpParams().set('url', urlProfile);
+
+    return this.httpClient.get<UserFollow[]>(this.apiUrl + '/getFollowings', {params});
   }
 }
